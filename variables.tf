70e9475eeb579df7348e3a26392f71e24c0294f6
shilparg <shilparg_2000@yahoo.com>
2025-11-15 00:07:30 +0800

    devsecops 3.1 new

--- variables.tf
+++ variables.tf
@@ -7,17 +7,17 @@ variable "aws_region" {
   }
 }
 
-variable "aws_access_key" {
-  description = "AWS Access Key ID"
-  type        = string
-  sensitive   = true
-}
+# variable "aws_access_key" {
+#   description = "AWS Access Key ID"
+#   type        = string
+#   sensitive   = true
+# }
 
-variable "aws_secret_key" {
-  description = "AWS Secret Access Key"
-  type        = string
-  sensitive   = true
-}
+# variable "aws_secret_key" {
+#   description = "AWS Secret Access Key"
+#   type        = string
+#   sensitive   = true
+# }
 
 variable "s3_bucket" {
   description = "S3 bucket for Terraform state"
